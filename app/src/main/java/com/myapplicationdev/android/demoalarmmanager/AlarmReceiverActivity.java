e7320ecd55138a1027b068e085606094b327c1fa
19002765 <19002765@myrp.edu.sg>
2021-08-03 11:06:30 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,30 @@
+package com.myapplicationdev.android.demoalarmmanager;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.myapplicationdev.android.demoalarmmanager.databinding.ActivityAlarmReceiverBinding;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+
+}
